995c933b6e90f2da8d6b7a6e1551ca9dc7053ff9
aminamin13 <amin.hassan.1324@gmail.com>
2023-08-11 14:35:43 +0300

    3 commit

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 
 
 var _ServicesContainer = document.getElementById('ServicesContainer')
-for (let i = 0; i < 4 ; i++) {
-    var _icon = "";
-    var _title = '';
-    
-    if (i == 0) {
-        _icon = 'map';
-        _title = "Location";
-    }
-    if (i == 1) {
-        _icon = 'envelope';
-        _title = "Email";
 
-    }
-    if (i == 2) {
-        _icon = 'alexa';
-        _title = "Alexa";
+fetch('http://localhost:3000/services')
+    .then(response => response.json())
+    .then(json => {
 
-    }
-    if (i == 3) {
-        _icon = 'briefcase';
-        _title = "Briefcase";
 
-    }
-    var content = `
-    <div class="icon"><i class="bi bi-${_icon}"></i></div>
-    <h4 class="title"><a href="">${_title}</a></h4>
-    <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+        for (let i = 0; i < json.length; i++) {
+
+            var content = `
+    <div class="icon"><i class="bi bi-${json[i].icon}"></i></div>
+    <h4 class="title"><a href="">${json[i].name}</a></h4>
+    <p class="description">${json[i].description}</p>
     `
-    let _div = document.createElement('div')
-    _div.innerHTML = content;
-    _div.className = 'col-lg-4 col-md-6 icon-box';
-    _ServicesContainer.appendChild(_div)
-}   
+            let _div = document.createElement('div')
+            _div.innerHTML = content;
+            _div.className = 'col-lg-4 col-md-6 icon-box';
+            _ServicesContainer.appendChild(_div)
+        }
+
+
+    });
+
+
